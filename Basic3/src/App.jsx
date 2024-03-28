2e8978458d16015ac63a1d3249463a528f5ac76f
RomanKunal <0221csds107@niet.co.in>
2024-03-28 23:47:06 +0530

    got it

---
+++ Basic3/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Headers.jsx"
+import UserInput from "./components/UserInput.jsx"
+function App() {
+  return (
+    <>
+    <Header />
+    <UserInput/>
+    </>
+  )
+}
+
+export default App
---
+++ Basic3/src/components/UserInput.jsx
@@ -0,0 +1,25 @@
+export default function UserInput(){
+    return <section id="user-input">
+        <div className="input-group">
+            <p>
+                <label>A</label>
+                <input type="number"/>
+            </p>
+            <p>
+                <label>B</label>
+                <input type="number"/>
+            </p>
+        </div>
+        <div className="input-group">
+            <p>
+                <label>C</label>
+                <input type="number"/>
+            </p>
+            <p>
+                <label>D</label>
+                <input type="number"/>
+            </p>
+        </div>
+    </section>
+    
+}
