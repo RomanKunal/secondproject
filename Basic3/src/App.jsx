813e49c2e87c65f949ca28259d9486919d0872fc
RomanKunal <0221csds107@niet.co.in>
2024-04-12 03:53:34 +0530

    push

--- Basic3/src/App.jsx
+++ Basic3/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Headers.jsx"
 import UserInput from "./components/UserInput.jsx"
+import Results from "./components/Results.jsx";
 function App() {
+  const [userInput,setuserinput]=
+    useState({
+        initialinvestment:10000,
+        annualinvestment:1200,
+        expectedreturn:6,
+        duration:10
+
+    });
+
+
+    function handlechange(inputidentifier,newvalue){
+      setuserinput(prevUserinput=>{
+          return {
+              ...prevUserinput,
+              [inputidentifier]:newvalue
+          };
+      });
+
+  }
   return (
     <>
     <Header />
-    <UserInput/>
+    <UserInput UserInput={userInput} onChange={handlechange}/>
+    <Results input={userInput}/>
     </>
   )
 }
--- Basic3/src/components/UserInput.jsx
+++ Basic3/src/components/UserInput.jsx
@@ -1,23 +1,38 @@
-export default function UserInput(){
+import { useState } from "react"
+
+
+
+export default function UserInput({ onChange, userInput}){
+
+    
+
+    
     return <section id="user-input">
         <div className="input-group">
             <p>
-                <label>A</label>
-                <input type="number"/>
+                <label>Initial Investment</label>
+                <input type="number" 
+                value={userInput.initialinvestment}hy2hgv x
+                required onChange={(event)=>onChange('initialinvestment',event.target.value)}/>
             </p>
             <p>
-                <label>B</label>
-                <input type="number"/>
+                <label>Annual Investment</label>
+                <input type="number" value={userInput.annualinvestment}
+                required onChange={(event)=>onChange('annualinvestment',event.target.value)} />
             </p>
         </div>
         <div className="input-group">
             <p>
-                <label>C</label>
-                <input type="number"/>
+                <label>Expected Return</label>
+                <input type="number" value={userInput.expectedreturn}
+                required onChange={(event)=>onChange('expectedreturn',event.target.value)}
+                />
             </p>
             <p>
-                <label>D</label>
-                <input type="number"/>
+                <label>Duration</label>
+                <input type="number" value={userInput.duration}
+                required onChange={(event)=>onChange('duration',event.target.value)}
+                />
             </p>
         </div>
     </section>
